1f4f625d782ab310f693c9db1842a2dfee20155f
Mahak Goel <mahak.s.goel@gmail.com>
2026-07-08 00:59:15 +0530

    Leet238_ProductExceptSelf.java

---
+++ Arrays/Leet238_ProductExceptSelf.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        int product = 1;
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            answer[i] *= product;
+            product *= nums[i];
+        }
+
+        return answer;
+    }
+}
