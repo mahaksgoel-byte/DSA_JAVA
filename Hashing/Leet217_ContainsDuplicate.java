0e78f5f4bc0f9f79c5adc7215837f36a7b6fffc1
Mahak Goel <mahak.s.goel@gmail.com>
2026-07-08 15:24:49 +0530

    Leet217_ContainsDuplicate.java

--- Hashing/Leet217_ContainsDuplicate.java
+++ Hashing/Leet217_ContainsDuplicate.java
@@ -1,3 +1,4 @@
+//HashMap Solution
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -7,6 +8,20 @@ class Solution {
                 map.put(nums[i], 1);
             }
 
+            else return true;
+        }
+
+        return false;
+    }
+}
+
+//HashSet Solution
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(); 
+
+        for(int i = 0; i < nums.length; i++){
+            if(set.isEmpty() || !set.contains(nums[i])) set.add(nums[i]);
             else return true;
         }
 
