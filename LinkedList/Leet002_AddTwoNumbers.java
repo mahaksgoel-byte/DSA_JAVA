d22fbc9795b4f7256c7afd294593187f3ead9709
Mahak Goel <mahak.s.goel@gmail.com>
2026-07-10 22:04:21 +0530

    Leet002_AddTwoNumbers.java

---
+++ LinkedList/Leet002_AddTwoNumbers.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, temp = null;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+
+            carry = 0;
+
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            if(sum > 9){
+                carry = 1;
+                sum = sum % 10;
+            }
+
+            ListNode newNode = new ListNode(sum);
+
+            if(head == null){
+                head = newNode;
+                temp = head;
+            }
+
+            else{
+                temp.next = newNode;
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+}
