d4caaafd8caaafe08f72df3b85a13c9b62bdf456
Mahak Goel <mahak.s.goel@gmail.com>
2026-07-13 00:19:02 +0530

    Leet024_SwapNodesInPairs.java

--- LinkedList/Leet024_SwapNodesInPairs.java
+++ LinkedList/Leet024_SwapNodesInPairs.java
@@ -9,36 +9,14 @@
  * }
  */
 class Solution {
-    public void pairReverse(ListNode back, ListNode prev, ListNode curr, ListNode front){
-
-        if(prev == null || curr == null) return;
-
-        curr.next = prev;
-        prev.next = front;
-        back.next = curr;
-
-        back = prev;
-        prev = front;
-
-        if(prev != null){
-            curr = prev.next;
-        }
-
-        if(curr != null){
-            front = curr.next;
-        }
-
-        pairReverse(back, prev, curr, front);
-    }
-
     public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
 
-        ListNode dummy = new ListNode(-1, head);
-        ListNode back = dummy, prev = head, curr = head.next;
+        ListNode second = head.next;
 
-        pairReverse(back, prev, curr, curr.next);
+        head.next = swapPairs(second.next);
+        second.next = head;
 
-        return dummy.next;
+        return second;
     }
 }
