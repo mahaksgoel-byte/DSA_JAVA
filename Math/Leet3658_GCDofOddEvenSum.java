016a8a38814dfdbfd91ea2ed41ea9033be2501c4
Mahak Goel <mahak.s.goel@gmail.com>
2026-07-16 14:17:13 +0530

    Leet3658_GCDofOddEvenSum.java

--- Math/Leet3658_GCDofOddEvenSum.java
+++ Math/Leet3658_GCDofOddEvenSum.java
@@ -1,3 +1,4 @@
+// SOLUTION 1
 class Solution {
     public int gcdOfOddEvenSums(int n) {
         int sumOdd = 0, sumEven = 0;
@@ -16,5 +17,12 @@ class Solution {
         }
 
         return a;
+    }
+}
+
+// SOLUTION 2 - sum of first n odd nums is n * n and first even nums is n * (n + 1) hence GCD is n
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n; 
     }
 }
