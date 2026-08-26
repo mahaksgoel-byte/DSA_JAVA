77bdec975ebe7d598c998536a9e27b1b0d6dd989
Mahak Goel <mahak.s.goel@gmail.com>
2026-08-26 18:48:15 +0530

    Leet523_ContinuousSubarraySum.java

---
+++ Hashing/Leet523_ContinuousSubarraySum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            int val = sum % k;
+
+            if(map.containsKey(val)){
+                if(i - map.get(val) >= 2) 
+                    return true;
+            }
+
+            else map.put(val, i);
+        }
+
+        return false;
+    }
+}
