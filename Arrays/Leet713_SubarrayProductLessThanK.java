133c2f58657bd358d1726c7b2ce6cc68fadbb938
Mahak Goel <mahak.s.goel@gmail.com>
2026-09-02 16:20:43 +0530

    Leet713_SubarrayProductLessThanK.java

---
+++ Arrays/Leet713_SubarrayProductLessThanK.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k <= 1) return 0;
+
+        int product = 1, count = 0, i = 0, j = 0;
+
+        while(j < nums.length){
+            product *= nums[j];
+
+            while(product >= k) product /= nums[i++];
+
+            count += j - i + 1;
+            j++;
+        }
+
+        return count;
+    }
+}
