a3e578e24769b1dcf1962dee08ee698f6f0ace1e
Mahak Goel <mahak.s.goel@gmail.com>
2026-09-03 09:43:47 +0530

    Leet3876_ConstructUniformParityArray2.java

---
+++ Math/Leet3876_ConstructUniformParityArray2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;;
+        for(int i : nums1) min = Math.min(min, i);
+
+        if(min % 2 != 0) return true; //odd
+
+        for(int i : nums1){
+            if(i % 2 != 0) return false; // all even
+        }
+
+        return true;
+    }
+}
