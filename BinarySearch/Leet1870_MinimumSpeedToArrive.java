60c17d527f9317110f9c2367badc33bdb6a521e1
Mahak Goel <mahak.s.goel@gmail.com>
2026-09-17 00:09:55 +0530

    Leet1870_MinimumSpeedToArrive.java

--- BinarySearch/Leet1870_MinimumSpeedToArrive.java
+++ BinarySearch/Leet1870_MinimumSpeedToArrive.java
@@ -1,11 +1,11 @@
 class Solution {
-    public boolean countHours(int[] dist, int speed, double hour){
+    public boolean isValid(int[] dist, int speed, double hour){
         double hours = 0;
 
         for(int i = 0; i < dist.length - 1; i++){
-            hours += (dist[i] + speed - 1) / speed;
+            hours += (dist[i] + speed - 1) / speed; //integer ceil method
 
-            if(hours > hour) return false;
+            if(hours > hour) return false; 
         }
 
         hours += (double) dist[dist.length - 1] / speed;
@@ -15,23 +15,25 @@ class Solution {
 
     public int minSpeedOnTime(int[] dist, double hour) {
         int n = dist.length;
-        if(hour <= n - 1) return -1;
+        if(hour <= n - 1) return -1; // atleast n - 1 hr required because n - 1 trains will take 1 hr each
 
         int max = 0;
         for(int i : dist){
             if(max < i) max = i;
-        } 
+        }
 
-        int lastSpeed = (int) Math.ceil(dist[n - 1] / (hour - (n - 1)));
+        int lastSpeed = (int) Math.ceil(dist[n - 1] / (hour - (n - 1))); //speed required for last train
         if(max < lastSpeed) max = lastSpeed;
- 
+
+        if(hour == n) return max; // use maximum speed if hours is equal to trains
+
         int left = 1, right = max;
 
         while(left < right){
             int mid = left + (right - left) / 2;
 
-            if(!countHours(dist, mid, hour)) left = mid + 1;
-            else right = mid;
+            if(isValid(dist, mid, hour)) right = mid;
+            else left = mid + 1;
         }
 
         return left;
